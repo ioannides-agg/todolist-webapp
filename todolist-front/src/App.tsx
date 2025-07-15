10b3a62c52dbe36d8210990792c127b9c714c423
ioannides-agg <ioannides.agg@gmail.com>
2025-07-15 03:25:29 +0300

    bundled logic to page, to begin working on login page

--- todolist-front/src/App.tsx
+++ todolist-front/src/App.tsx
@@ -1,30 +1,8 @@
-import AddTodoForm from "./components/AddTodoForm";
-import TodoList from "./components/TodoList";
-import TodoSummary from "./components/TodoSummary";
-import useTodos from "./hooks/useTodos";
+import TodoPage from "./pages/TodoPage"
 
 function App() {
-  const { todos,
-    setTodoCompleted,
-    setTodoDeleted,
-    addTodo,
-    deleteAllCompletedTodos } = useTodos();
-
   return (
-    <main className="py-10 h-screen space-y-5 overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-        <AddTodoForm onSubmit={addTodo} />
-        <TodoList
-          todos={todos}
-          onCompletedChange={setTodoCompleted}
-          onDeletedChange={setTodoDeleted}
-        />
-      </div>
-      {todos.length > 0 && (
-        <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
-      )}
-    </main>
+    <TodoPage />
   )
 }
 
---
+++ todolist-front/src/pages/TodoPage.tsx
@@ -0,0 +1,29 @@
+import AddTodoForm from "../components/AddTodoForm";
+import TodoList from "../components/TodoList";
+import TodoSummary from "../components/TodoSummary";
+import useTodos from "../hooks/useTodos";
+
+export default function TodoPage() {
+    const { todos,
+        setTodoCompleted,
+        setTodoDeleted,
+        addTodo,
+        deleteAllCompletedTodos } = useTodos();
+
+    return (
+        <main className="py-10 h-screen space-y-5 overflow-y-auto">
+            <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+            <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+                <AddTodoForm onSubmit={addTodo} />
+                <TodoList
+                    todos={todos}
+                    onCompletedChange={setTodoCompleted}
+                    onDeletedChange={setTodoDeleted}
+                />
+            </div>
+            {todos.length > 0 && (
+                <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
+            )}
+        </main>
+    )
+}
