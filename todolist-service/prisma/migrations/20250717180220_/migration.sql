f47f6d5770613782c2b57b71401834be26dd4feb
ioannides-agg <ioannides.agg@gmail.com>
2025-07-18 00:15:54 +0300

    removed migrations to add them to .gitignore

--- todolist-service/prisma/migrations/20250717180220_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "todo" (
-    "id" SERIAL NOT NULL,
-    "owner" INTEGER NOT NULL,
-    "title" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
-);
